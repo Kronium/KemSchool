5ff0db6e9cb415eac9ab2422b4a9b4079d973cf6
Hades <Kronium>
2022-02-17 00:02:16 -0600

    Explained better

--- src/Float.java
+++ src/Float.java
@@ -1,4 +1,5 @@
 import java.math.BigDecimal;
+import java.util.Scanner;
 
 public class Float {
     /*
@@ -12,6 +13,7 @@ public class Float {
       }
 
      */
+    //scanner
 
     /*
     first numbie
@@ -19,12 +21,29 @@ public class Float {
     public static void main(String[] args) {
 
         // Telling the system to truncate ( a function defined below ) a decimal
-        System.out.println(truncateDecimal(10.5, 2));
+        //Object 1
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Number 1: ");
+        String numberOne = myObj.nextLine();
+
+        System.out.println(truncateDecimal(Double.parseDouble(numberOne), 2));
+
+        Scanner myObj2 = new Scanner(System.in);
+        System.out.println("Number 2: ");
+        String numberTwo = myObj2.nextLine();
+
+        System.out.println(truncateDecimal(Double.parseDouble(numberTwo), 2));
+        Scanner myObj3 = new Scanner(System.in);
+        System.out.println("Number 3: ");
+        String numberThree = myObj3.nextLine();
+
+        System.out.println(truncateDecimal(Double.parseDouble(numberThree), 2));
+
 
     }
     //////////////////////////////////////////////
     //Telling the system how to truncate a decimal
-    //This is the function refered to in System.out.println(truncateDecimal
+    //This is the function referred to in System.out.println(truncateDecimal
     //
     //Double is your input number or x
     //Number of decimals is the number you want to go over so 2 = .00 and 3 = .000
@@ -33,13 +52,13 @@ public class Float {
     private static BigDecimal truncateDecimal(double x, int numberofDecimals) {
         if ( x > 0)
         {
-            //This is telling it to return a string from the nomber x over 2 decimals if below 0
+            //This is telling it to return a string from the number x over 2 decimals if below 0
 
             return new BigDecimal(String.valueOf(x)).setScale(numberofDecimals, BigDecimal.ROUND_FLOOR);
         }
         else
         {
-            //This is telling it to return a string from the nomber x over 2 decimals if above 0
+            //This is telling it to return a string from the number x over 2 decimals if above 0
 
             return new BigDecimal(String.valueOf(x)).setScale(numberofDecimals, BigDecimal.ROUND_CEILING);
         }
