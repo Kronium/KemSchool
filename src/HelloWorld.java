642d4cbed538519431662dae845143d25516e3f7
Hades <Kronium>
2022-02-16 23:45:13 -0600

    Explained better

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,9 +3,17 @@ import java.util.*;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        //This is outputting the text hello world into the console
+
+        System.out.println("Hello, world!");
+
+        //For multiple lines you would enter the below code
+        //This will put both hello and world on different lines.
         //Hello
         System.out.println("Hello,");
+
         //World
+
         System.out.println("world.");
     }
 }
